2c3a04b3ccbf525763b45c668ec0d4238b81a3a9
Dmitriy Karasev <>
2019-02-27 15:39:26 +0300

    🎨 improve example

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 const locale = 'ru';
 
-console.log(require(`moment/locale/${locale}`));
+// work
+// console.log(require('moment/locale/ru'));
+
+// work
+// console.log(require(`moment/locale/${locale}`));
+
+// work
 // (async () => { console.log(await(import(`moment/locale/${locale}`))); })();
 
 export default () => '';
